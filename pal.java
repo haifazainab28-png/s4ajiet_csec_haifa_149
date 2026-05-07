44cd479e9c62ce10aa34a280116c9c5af43d3847
haifazainab28-png <haifazainab28@gmail.com>
2026-05-07 12:00:16 +0530

    Update and rename rev.java to pal.java

--- pal.java
+++ pal.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class rev{
+public class pal{
     public static void  main(String[] args){
         Scanner s= new Scanner(System.in);
         System.out.println("Enter the number :");
